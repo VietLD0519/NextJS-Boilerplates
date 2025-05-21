7eb0d05abffb1eff836ec39acf693a9ddcb8d1bf
Lương Đức Việt <134918488+VietLD0519@users.noreply.github.com>
2025-05-21 08:05:42 +0700

    Add files via upload

---
+++ setup-commands.sh
@@ -0,0 +1,10 @@
+# Tạo dự án Next.js
+npx create-next-app@latest nextjs-mui-clean --typescript --tailwind --eslint --app --src-dir --import-alias "@/*"
+
+# Di chuyển vào thư mục dự án
+cd nextjs-mui-clean
+
+# Cài đặt các thư viện cần thiết
+npm install @mui/material @mui/icons-material @emotion/react @emotion/styled
+npm install react-hook-form @hookform/resolvers zod
+npm install zustand axios jwt-decode
